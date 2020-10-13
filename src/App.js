f3a3613fadd9c2ca619b010116f01f450bb077e6
Reez <reez74@gmail.com>
2020-10-13 19:59:07 +0300

    first commit 13.10.20

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, NavLink, Route} from "react-router-dom";
+
+const Menu = () => {
+  return <ul>
+    <li><NavLink to="/profile">Профиль</NavLink></li>
+    <li><NavLink to="/settings">Настройки</NavLink></li>
+    <li><NavLink to="/users">Пользователи</NavLink></li>
+  </ul>
+}
+
+const Profile = () => {
+  return <div className="row">
+    <div className="col-2"></div>
+
+  </div>
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <BrowserRouter>
+        <div className="row">
+          <div className="col-3">
+            <Menu/>
+          </div>
+          <div className="col-9">
+            <Route path="/profile" render={()=>{return <Profile/>}}/>
+            <Route path="/settings" render={()=>{return <h1>Настройки</h1>}}/>
+            <Route path="/users" render={()=>{return <h1>Пользователи</h1>}}/>
+          </div>
+        </div>
+       </BrowserRouter>
     </div>
   );
 }
